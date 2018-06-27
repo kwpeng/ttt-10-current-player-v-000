2a4ea3c074da32c540242b600f1fb2a7923def6b
bluebeadlily@gmail.com <bluebeadlily@gmail.com>
2018-06-27 02:19:49 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,10 +2,10 @@ def turn_count(board)
   counter = 1
 board.each do |element|
   if element == "X" || element == "O"
-    puts "#{counter}"
+    
     counter += 1
   end
-   puts "#{counter}"
+   puts "#{counter}""
 end
 end
   turn_count(["O", " ", " ", " ", "X", " ", " ", " ", "X"])
