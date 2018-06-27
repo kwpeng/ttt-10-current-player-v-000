a45388e36ea0b4885b801a7b5a0e2f866c6735dc
bluebeadlily@gmail.com <bluebeadlily@gmail.com>
2018-06-27 02:26:36 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,4 +1,4 @@
- board = ["O", " ", " ", " ", "X", " ", " ", " ", "X"]
+
 
 def turn_count(board)
   counter = 0
@@ -12,11 +12,11 @@ end
 
   
   def current_player(board)
-  if turn_count % 2 == 0
+  if turn_count(board) % 2 == 0
     puts "X"
   else 
    puts "O"
   end
 end
 
-current_player(board)
+current_player(["O", " ", " ", " ", "X", " ", " ", " ", "X"])
