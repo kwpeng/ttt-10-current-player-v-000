884f1a196763a86df1b052649175237e60b2fc1a
bluebeadlily@gmail.com <bluebeadlily@gmail.com>
2018-06-27 01:46:12 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,3 +1,9 @@
-def turn_count(board(array))
+turn_count.each do |element|
   counter = 0
+  if 
   
+  
+  
+  primary_colors.each do |color|
+  puts "Primary Color #{color} is #{color.length} letters long."
+end
