d7d743ef1cc821769a075acea1f7bb98e7cf7f05
bluebeadlily@gmail.com <bluebeadlily@gmail.com>
2018-06-27 02:01:55 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -6,9 +6,12 @@ board.each do |element|
   end
   
   
-  
-  
-  
+  def current_player(game_board)
+  if turn_count % 2 == 0
+    board(array) = "X"
+  else 
+    board(array) = "O"
+    
   primary_colors.each do |color|
   puts "Primary Color #{color} is #{color.length} letters long."
 end
