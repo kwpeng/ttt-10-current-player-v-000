ab6b1a62f3eb8dc77540b1f26356f517f519a21f
bluebeadlily@gmail.com <bluebeadlily@gmail.com>
2018-06-27 02:15:36 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,8 +2,8 @@ def turn_count(board)
   counter = 0
 board.each do |element|
   if element == "X" || element == "O"
-    counter += 1
-    puts #{counter}
+    counter += 1"
+    puts #{counter}"
   end
    puts #{counter}
 end
